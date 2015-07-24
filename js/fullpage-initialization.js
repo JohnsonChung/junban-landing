21747bdaa13f39fd56b657f76cad1b5afea25ea4
Johnson <johnson780510@gmail.com>
2015-07-24 23:48:54 +0800

    update

--- js/fullpage-initialization.js
+++ js/fullpage-initialization.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         //Navigation
         menu: '#menu',
-        anchors:['top', 'what-junban', 'store-problem', 'why-junban', 'how-junban', 'try-junban', 'pricing', 'ask-junban', 'spec-junban'],
+        anchors: ['top', 'what-junban', 'store-problem', 'why-junban', 'how-junban', 'try-junban', 'pricing', 'ask-junban', 'spec-junban'],
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['what-junban', 'store-problem', 'why-junban', 'how-junban', 'try-junban', 'pricing', 'ask-junban', 'spec-junban'],
@@ -13,6 +13,7 @@ $(document).ready(function() {
         css3: true,
         scrollingSpeed: 500,
         autoScrolling: false,
+        fitToSection: false,
         scrollBar: false,
         easing: 'swing',
         easingcss3: 'linear',
@@ -32,23 +33,65 @@ $(document).ready(function() {
         //Design
         controlArrows: true,
         verticalCentered: true,
-        resize : true,
-        sectionsColor : ['#fff', '#F09C35', '#ddd', '#F09C35','#f5f0e6','#F09C35','#FFFFFF','#5cb85c','#fff'],
+        resize: false,
+        sectionsColor: ['#fff', '#F09C35', '#F1F1F1', '#F09C35', '#f5f0e6', '#F09C35', '#FFFFFF', '#5cb85c', '#fff'],
         paddingTop: '52px',
         paddingBottom: '3em',
         fixedElements: '#header',
-        responsiveWidth: 1280,
+        responsiveWidth: 0,
+        responsiveHeight: 0,
 
         //Custom selectors
         sectionSelector: '.section',
         slideSelector: '.slide',
 
         //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){},
-        afterResize: function(){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+        /**
+         * Loaded
+         */
+        afterLoad: function(anchorLink, index) {
+
+        },
+        /**
+         * Leave Section
+         */
+        onLeave: function(index, newIndex, direction) {
+            console.log("Leaving: " + index + " | into: " + newIndex);
+            if (newIndex === 2 && direction == 'down') {
+
+            } else if (index == 3 && direction == 'up') {
+                // $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
+            }
+
+            // $('img').toggleClass('active', (index == 2 && direction == 'down') || (index == 4 && direction == 'down'));
+
+            $("#fullpage .section:nth-child(2)").find('p').toggleClass("animated pulse", newIndex === 2);
+            $("#fullpage .section:nth-child(3)").find('h1').toggleClass("animated pulse", newIndex === 3);
+            $("#fullpage .section:nth-child(4)").find('h1').toggleClass("animated fadeInLeft", newIndex === 4 && direction === 'down');
+            $("#fullpage .section:nth-child(5)").find('h1').toggleClass("animated fadeInLeft", newIndex === 5 && direction === 'down');
+            $("#fullpage .section:nth-child(6)").find('.btn').toggleClass("animated pulse", newIndex === 6);
+
+
+            if (newIndex === 7 && direction === 'down') {
+                var target = $("#fullpage .section:nth-child(7)").find('.pricing-table');
+                var i = 0;
+
+                function loop() {
+                    target.eq(i).toggleClass("animated pulse", newIndex === 7 && direction === 'down');
+                    if (i < target.length) setTimeout(loop, 800);
+                    i++;
+                }
+                setTimeout( loop , 1500 );
+            }
+
+            // $('#staticImg').toggleClass('moveDown', newIndex == 4);
+            // $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
+        },
+        /**
+         * Rendered
+         *      Needed to maintain the fixed position of the element
+         *      due to problems using translate3d and fixed elements.
+         */
+        afterRender: function() {}
     });
 });
